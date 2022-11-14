581e35ee06fa32e1ffe63f632ef30474dff3c817
unknown <snehalbaviskar2@gmail.com>
2022-11-14 15:08:22 +0000

    added dynamic changes

--- app.js
+++ app.js
@@ -20,3 +20,39 @@ function Description() {
     var myWindow=window.open("Product-desc.html","_self");
 }
 
+function getproductdes(productid){
+    
+    $(document).ready(()=>{
+        $.getJSON('products.json', (data)=>{
+            data.forEach((product)=>{
+                console.log(product.title+"      "+productid);
+               /* if((product.title==document.getElementById("search").value)|| (product.title==document.getElementById("product_img").value))
+                {*/
+                    console.log("Hi");
+                    location.href="./Product-desc.html?id="+productid;
+                    
+               // }
+            });
+        });
+    });
+
+
+}
+function f1(){
+    
+    $(document).ready(()=>{
+        $.getJSON('products.json', (data)=>{
+            data.forEach((product)=>{
+                console.log(product.title+" "+document.getElementById("search").value);
+                if(product.title==document.getElementById("search").value)
+                {
+                    
+                    location.href="./Product-desc.html?id="+product.id;
+                    
+                }
+            });
+        });
+    });
+
+
+}
